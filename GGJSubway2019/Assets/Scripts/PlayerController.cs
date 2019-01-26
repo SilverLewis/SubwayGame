67f0854c1a624bac1fc3152f7df6837c9d9d8284
Silver Lewis <silverlewis@gmail.com>
2019-01-26 15:17:07 -0500

    basic playercontroller

---
+++ GGJSubway2019/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    private Rigidbody2D rb;
+    private float previousMovement;
+
+    void Start()
+    {
+        previousMovement = 0;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        print ( movement);
+        bool stillPressing = true;
+
+        if (Mathf.Abs(previousMovement) > Mathf.Abs(movement) && Mathf.Sign(previousMovement) == Mathf.Sign(movement))
+        {
+            stillPressing = false;
+        }
+
+        previousMovement = movement;
+
+        if (Mathf.Abs(movement) > 0 &&stillPressing){
+            movement = Mathf.Sign(movement)*1;        
+            rb.velocity = new Vector2(movement * moveSpeed, 0);
+        }
+        else
+            rb.velocity = new Vector2(0, 0);
+
+
+
+
+    }
+}
