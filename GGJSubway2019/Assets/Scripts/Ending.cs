f6ff180a96bd340f23689416546dc5ba930e206f
Silver Lewis <silverlewis@gmail.com>
2019-01-27 12:04:52 -0500

    endings are created

---
+++ GGJSubway2019/Assets/Scripts/Ending.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ending : MonoBehaviour
+{
+    public Text peopleTextBox, endingTextBox;
+    public Image blackBox;
+    public string msg;
+    public TrainController train;
+    // Start is called before the first frame update
+
+    private void ending()
+    {
+        peopleTextBox.enabled = false;
+        endingTextBox.enabled = true;
+        blackBox.gameObject.SetActive(true);
+        endingTextBox.text = msg;
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && collider.CompareTag("Player") && !train.playerOnBoard)
+        {
+            print("endGame");
+            ending();
+        }
+
+    }
+}
