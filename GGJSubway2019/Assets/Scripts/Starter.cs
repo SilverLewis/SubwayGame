48633ea7c863862921340521a80d82fa445a2624
Silver Lewis <silverlewis@gmail.com>
2019-01-27 15:03:47 -0500

    starting works

---
+++ GGJSubway2019/Assets/Scripts/Starter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Starter : MonoBehaviour
+{
+    public PlayerController player;
+    public Image image;
+    public Text text;
+
+    void Start()
+    {
+        player.enabled = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown) {
+            player.enabled = true;
+            image.enabled = false;
+            text.enabled = false;
+            gameObject.SetActive(false);
+        }
+    }
+}
